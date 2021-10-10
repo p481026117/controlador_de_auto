9ad59669909b89dea496742fcba1a62df0f452ba
p48102611 <p481026110@gmail.com>
2021-10-10 05:40:09 -0400

    Probando funcion avanzar

--- src/controladorAuto.js
+++ src/controladorAuto.js
@@ -1,4 +1,21 @@
-
+function Avanzar(posicion, grilla) {
+  let x,y;
+  let filas = grilla[0];
+  let columnas = grilla[1];
+  if(posicion.direccion === "N" && posicion.j + 1 < filas){
+    posicion.j++;
+  }
+  if(posicion.direccion === "S" && posicion.j - 1 >= 0){
+    posicion.j--;
+  }
+  if(posicion.direccion === "E" && posicion.i + 1 < columnas){
+    posicion.i++;
+  }
+  if(posicion.direccion === "O" && posicion.i - 1 >= 0){
+    posicion.i--;
+  }
+  return posicion;
+}
 
 class Posicion {  // Create a class
   constructor(i,j,direccion) {  // Class constructor
@@ -14,5 +31,6 @@ function PositionInicial(i,j,direccion) {
 // var list = [1,2,3,4,5].filter(function(x){ return x < 4; })
 //                .map(function(x) { return 'foo ' + x; });
 module.exports = {
-  PositionInicial
+  PositionInicial,
+  Avanzar
 }
--- src/controladorAuto.test.js
+++ src/controladorAuto.test.js
@@ -5,6 +5,25 @@ describe("CONTROLADOR DE AUTO TEST", () => {
   it("Deberia devolver un Objeto posicion ingresada como parametros a la funcion.", () => {
     expect(libreria.PositionInicial(1,2,"N")).toEqual({"direccion": "N", "i": 1, "j": 2});
   });
-  
+  it("Deberia devolver un objeto {'direccion': 'N', 'i': 1, 'j': 1} de la funcion Avanzar hacia el norte", () => {
+    let posicion = libreria.PositionInicial(1,0,"N");
+    let grilla = [3,3];
+    expect(libreria.Avanzar(posicion,grilla)).toEqual({"direccion": "N", "i": 1, "j": 1});
+  });
+  it("Deberia devolver un objeto {'direccion': 'S', 'i': 1, 'j': 1} de la funcion Avanzar hacia el sur", () => {
+    let posicion = libreria.PositionInicial(1,1,"S");
+    let grilla = [3,3];
+    expect(libreria.Avanzar(posicion,grilla)).toEqual({"direccion": "S", "i": 1, "j": 0});
+  });
+  it("Deberia devolver un objeto {'direccion': 'E', 'i': 1, 'j': 1} de la funcion Avanzar hacia el este", () => {
+    let posicion = libreria.PositionInicial(1,1,"E");
+    let grilla = [3,3];
+    expect(libreria.Avanzar(posicion,grilla)).toEqual({"direccion": "E", "i": 2, "j": 1});
+  });
+  it("Deberia devolver un objeto {'direccion': 'O', 'i': 1, 'j': 1} de la funcion Avanzar hacia el norte", () => {
+    let posicion = libreria.PositionInicial(1,1,"O");
+    let grilla = [3,3];
+    expect(libreria.Avanzar(posicion,grilla)).toEqual({"direccion": "O", "i": 0, "j": 1});
+  });
 });
 
